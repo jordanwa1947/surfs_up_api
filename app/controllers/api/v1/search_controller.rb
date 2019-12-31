a637135e7b7c589b5530cacf59b9a20ed89f6af6
Jordan Whitten <jordanwa1947@gmail.com>
2019-12-31 16:53:07 -0700

    Use spot search method to find a group of matching spots and return json in the search controller

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::SearchController < ApplicationController
 
   def index
-    render json: SpotSerializer.new(Spot.search(params[:query]))
+    spot = Spot.search(params[:query])
+    render json: SpotSerializer.new(spot).serializable_hash
   end
 end
