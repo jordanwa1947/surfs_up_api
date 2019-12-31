1d0d0482bab0c60bd91720f026b5241230ece469
Jordan Whitten <jordanwa1947@gmail.com>
2019-12-31 16:46:14 -0700

    Add search route and surf route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/locations', to: 'spots#index'
       get '/search/locations', to: 'search#index'
+      get '/:spot_id/surf', to: 'surf#show'
     end
   end
 end
